5376012ac6ea922995cf18079f3e4cf592544458
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2020-08-15 20:02:16 -0500

    Adding demo project reactive.

---
+++ reactive-project-1/src/main/java/com/developervisits/reactive/ReactiveDemo1Application.java
@@ -0,0 +1,13 @@
+package com.developervisits.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemo1Application.class, args);
+	}
+
+}
---
+++ reactive-project-1/src/test/java/com/developervisits/reactive/ReactiveDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developervisits.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
